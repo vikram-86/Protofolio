bbb2c5c10cf4991f065fba750d4c3fa3419b581b
Vikram <vikram.sh86@gmail.com>
2020-10-27 10:43:19 +0100

    Initial Commit

---
+++ Protofolio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Protofolio
+//
+//  Created by Vikram on 27/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Protofolio/ProtofolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtofolioApp.swift
+//  Protofolio
+//
+//  Created by Vikram on 27/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct ProtofolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
